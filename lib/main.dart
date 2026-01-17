4f12bd66fcdf25f56d128d6164932a5a7ee565d0
Vloid <vloid137@gmail.com>
2026-01-17 07:29:10 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Web Minimal',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Hello Flutter Web')),
-        body: const Center(
-          child: Text('Hello, World!'),
+      home: CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++; // update state
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter App")),
+      body: Center(
+        child: Text(
+          'Angka: $_counter',
+          style: TextStyle(fontSize: 32),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: Icon(Icons.add),
       ),
     );
   }
