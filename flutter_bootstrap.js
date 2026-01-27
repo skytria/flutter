32fedee99a85b6bad7d259b8cf968074e86be60a
skytria <skytria@users.noreply.github.com>
2026-01-27 08:48:46 +0000

    deploy: 9a3ba31b3d4ee1f300fade452c7512f0ffe039e1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3523844340"
+    serviceWorkerVersion: "1744680912"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -9,7 +9,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
-"assets/fonts/MaterialIcons-Regular.otf": "deea0f5dba93813bade5621aec9b6b13",
+"assets/fonts/MaterialIcons-Regular.otf": "f669fab6a08288d24195db0d1fb9bdce",
 "assets/NOTICES": "8097b6b3024eb4bf45d02900dfb3eb91",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
@@ -25,9 +25,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/skwasm.wasm": "7e5f3afdd3b0747a1fd4517cea239898",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
-"flutter_bootstrap.js": "c87a6d2f20f90e1ac9c916aea4cc5693",
+"flutter_bootstrap.js": "320e5c913230c1d9109ecd6f807e792a",
 "version.json": "ca1b0e6f7520ed18490e9655c5ab8455",
-"main.dart.js": "b9718903ba484dfe675c1830c39e4935"};
+"main.dart.js": "6d0fa849088c9a93e519596b676d6dcd"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
