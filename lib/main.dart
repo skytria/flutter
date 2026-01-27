9a3ba31b3d4ee1f300fade452c7512f0ffe039e1
Vloid <vloid137@gmail.com>
2026-01-27 15:46:26 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart
+import 'pages/login_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,9 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'LOGIN PAGE',
+      debugShowCheckedModeBanner: false,
+      title: 'LOGIN PAGE',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: const LoginPage(),
     );
   }
 }
-
